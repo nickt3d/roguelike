2c0ffb82c36f9ff2c38b991b339eec888e8c9ef0
Nick Tremblay <ntremb@gmail.com>
2017-07-05 09:13:46 -0300

    workwork

--- draw.js
+++ draw.js
@@ -4,17 +4,36 @@ var c = canvas.getContext("2d");
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
 var running = true;
 
+
 //Loop
-function gameLoop(){
+function drawLoop(){
     requestAnimationFrame(drawLoop);
     if(running){
-    	$.getscript("draw.js"),function(){
-            draw();
-        };
+    	draw();    
     	running = false;
     }
 }
 
+//Drawing
+function draw(){
+	// Clear canvas at the start of each draw
+	c.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function drawMap(){
+	//draw all the map tiles
+}
+
+function drawFOW(){
+	//draw thhe fog of war
+}
+
+function drawEntities(){
+	//get entity list
+	//draw all entities that are in the view
+}
+
 window.onload = function(){
-	gameLoop();
+	drawLoop();
 };
+
